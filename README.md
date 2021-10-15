992936654f0a8b2b077b29358663de8c6293d3bf
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-10-15 17:32:30 -0400

    okay good to go

---
+++ README.md
@@ -0,0 +1,9 @@
+# Election Results
+## Introduction
+The goal of this project is to practice using the numpy and matplotlib python modules. Survey responses on a subset of a population of 10000 are provided, each preferenced either toward Ceballos or Kerrigan. Prediction % of outcome is calculated and compared against the actual outcome of 54%. Histograms of binomial distribution sample models are generated to show the likelyhood of Ceballos losing based on the current surveys, and if the number of surveys was instead 700 rather than just 33, but the predicted % outcome was still the same as in the original survey. The second binomial distribution histogram is created to demonstrate that predictions based on surveys increases when the number of people surveyed increases.
+## Technology
+* Python 3.8.11
+	* matplotlib 1.21.2
+	* numpy 3.4.3
+## Launch
+`python3 script.py`
---
+++ script.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#surveying a subset of population of 10000 people - assuming all of them will vote
+survey_responses = np.array(['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos'])
+
+#total # who vote ceballos prediction
+total_ceballos = len(survey_responses[survey_responses == 'Ceballos'])
+print(total_ceballos)
+
+#percent votes for ceballos prediction
+percentage_ceballos = np.mean(survey_responses == 'Ceballos')
+print(percentage_ceballos)
+
+#display histogram of binomial sample distribution for actual 54% outcome, numserveys (small num) and size = 10000 = population
+N=len(survey_responses)
+P=.54
+S=10000
+possible_surveys = np.random.binomial(N, P, S) / float(N)
+plt.hist(possible_surveys, range=(0,1), bins=20)
+
+#probability that ceballos loses according to binomial model
+ceballos_loss_surveys = np.mean(possible_surveys < .5)
+print(ceballos_loss_surveys)
+
+#hypothetically increase the number of surveys from 33 out of 10000 to 700 out of 10000
+N=700
+large_survey = np.random.binomial(N, P, S) / float(N)
+
+#display histogram of binomial sample distribution for actual 54% outcome, new larger hypothetical number of surveys, and size = 10000 = population
+plt.hist(large_survey, range=(0,1), bins=20, alpha=0.4)
+plt.show()
+
+#probability that ceballos loses baed on binomial model using produced with 700 surveys instead of 33
+ceballos_loss_new = np.mean(large_survey < .5)
+print(ceballos_loss_new)
