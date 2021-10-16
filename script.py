7b414b549a7f504454664fd335055dec3a7e6397
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-10-16 11:14:53 -0400

    more updates

--- script.py
+++ script.py
@@ -20,8 +20,7 @@ N=len(survey_responses)
 P=.54
 S=10000
 possible_surveys = np.random.binomial(N, P, S) / float(N)
-plt.hist(possible_surveys, range=(0,1), bins=20)
-
+plt.hist(possible_surveys, range=(0,1), bins=30)
 #probability that ceballos loses according to binomial model
 ceballos_loss_surveys = np.mean(possible_surveys < .5)
 print(ceballos_loss_surveys)
@@ -31,7 +30,13 @@ N=700
 large_survey = np.random.binomial(N, P, S) / float(N)
 
 #display histogram of binomial sample distribution for actual 54% outcome, new larger hypothetical number of surveys, and size = 10000 = population
-plt.hist(large_survey, range=(0,1), bins=20, alpha=0.4)
+plt.hist(large_survey, range=(0,1), bins=30, alpha=0.4)
+
+labels = ['33 surveys', '700 surveys']
+plt.legend(labels)
+plt.title('Sample Binomial Distribution for Votes for Cabellos')
+plt.xlabel("Percent Chance of Occurence")
+plt.ylabel("Votes for Cabellos")
 plt.show()
 
 #probability that ceballos loses baed on binomial model using produced with 700 surveys instead of 33
